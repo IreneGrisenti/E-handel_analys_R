540f293f6d8640304a7aa55d831d63be185fdaec
Henri Hörkkö <henri.h1979@gmail.com>
2026-04-02 13:06:41 +0200

    added report dataförståelse

--- scripts/01_data_understanding.R
+++ scripts/01_data_understanding.R
@@ -7,7 +7,7 @@ head(df)
 glimpse(df)
 
 colSums(is.na(df))
-
+  # visa data sett storlek
   # beskriva vilka typer av variabler som finns
   # identifiera saknade värden
   # beskriva kort vilka delar av datan som verkar viktigast för er analys
