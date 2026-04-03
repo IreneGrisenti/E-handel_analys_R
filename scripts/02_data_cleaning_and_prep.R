9a5cdca0330acb67763e66525be618a746dd1888
IreneGrisenti <irenegrisenti@proton.mem>
2026-04-03 10:03:09 +0200

    adjusted data types and strings

--- scripts/02_data_cleaning_and_prep.R
+++ scripts/02_data_cleaning_and_prep.R
@@ -1,11 +1,9 @@
 View(df)
 
-sapply(df, class)
-
-# Cleaning variables
-categorical_columns <- c("customer_segment", "customer_type", "region", "city", 
-                         "product_category", "product_subcategory", 
-                         "payment_method", "campaign_source")
+# Cleaning relevant variables
+categorical_columns <- c("customer_segment", "customer_type", "region", 
+                         "city", "product_category", "product_subcategory", 
+                         "campaign_source")
 
 checking_inconsistencies <- categorical_columns %>%
   set_names() %>%
@@ -16,20 +14,39 @@ checking_inconsistencies
 
 df_clean <- df %>% 
   mutate(
+    # making sure the id is standardized
     order_id = str_trim(order_id),
     order_id = str_to_upper(order_id),
-    order_date = as.Date(order_date),
     customer_id = str_trim(customer_id),
     customer_id = str_to_upper(customer_id),
+    
+    # making sure the date is data type
+    order_date = as.Date(order_date),
+    
+    # adjusted city spelling
     city = str_trim(city),
     city = str_to_title(city),
-    payment_method = str_trim(payment_method),
-    payment_method = str_to_title(payment_method),
+    city = str_replace_all(city, "Boras", "Borås"),
+    city = str_replace_all(city, "Gothenburg", "Göteborg"),
+    city = str_replace_all(city, "Linkoping", "Linköping"),
+    city = str_replace_all(city, "Malmo", "Malmö"),
+    city = str_replace_all(city, "Norrkoping", "Norrköping"),
+    city = str_replace_all(city, "Orebro", "Örebro"),
+    city = str_replace_all(city, "Vasteras", "Västerås"),
+    
+    # standardized campaign_source to avoid repeating values
     campaign_source = str_trim(campaign_source),
-    campaign_source = str_to_title(campaign_source)
+    campaign_source = str_to_title(campaign_source),
+    
+    # turning return into 0-1
+    returned = str_trim(returned),
+    returned = str_to_title(returned),
+    returned = if_else(returned == "Yes", 1, 0, missing = NA),
+    
+    # adjusting datatype for int cols
+    quantity = as.integer(quantity),
+    shipping_days = as.integer(shipping_days)
   )
-
-# fix special caracters in city?
 
 checking_inconsistencies <- categorical_columns %>%
   set_names() %>%
@@ -37,17 +54,15 @@ checking_inconsistencies <- categorical_columns %>%
   
 checking_inconsistencies
 
+glimpse(df_clean)
 
-
-
-
-  # hantera eventuella saknade värden på ett rimligt sätt
+# hantera eventuella saknade värden på ett rimligt sätt
     # city - 21
     # payment methos - 25 
     # campaign source 31 
     # discount pct 27
 
-  # skapa minst 2 nya variabler som hjälper er analys
+# skapa minst 2 nya variabler som hjälper er analys
   
     # Exempel:
       # ordervärde
